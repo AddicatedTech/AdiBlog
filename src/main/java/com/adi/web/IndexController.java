eb5363c246cff8494cec1087abeda337d594dbce
adi_glob <414213352@qq.com>
2020-11-17 13:30:42 +0800

    thymeleaf and frontend pages,error pages upload

--- src/main/java/com/adi/web/IndexController.java
+++ src/main/java/com/adi/web/IndexController.java
@@ -4,31 +4,32 @@
  * @Author: Addicated
  * @Date: 2020-11-16 17:57:25
  * @LastEditors: Addicated
- * @LastEditTime: 2020-11-16 20:46:01
+ * @LastEditTime: 2020-11-17 12:36:11
  */
 package com.adi.web;
-
-import javax.websocket.server.PathParam;
-
-import com.adi.NotFoundException;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-
 @Controller
 public class IndexController {
 
-    @GetMapping("/{id}/{name}")  // 在 url中接收参数的情况下使用pathVaribale注解来接受url中的变量
-    public String index(@PathVariable Integer id,@PathVariable String name) {
-        
+    @GetMapping("/") // 在 url中接收参数的情况下使用pathVaribale注解来接受url中的变量
+    public String index() {
+
         // int i = 9 / 0;
         // String blog = null;
         // if(blog == null){
-        //     throw new NotFoundException("博客找不到了~"); 
+        // throw new NotFoundException("博客找不到了~");
         // }
         System.out.println("----------index------------");
         return "index";
+    }
+
+    @GetMapping("/blog")
+    public String blog() {
+        System.out.println("----------blog------------");
+        return "blog";
     }
 }
